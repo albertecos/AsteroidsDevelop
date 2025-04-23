8ab808f46fe492745d25b5b6d7d82058f19f7913
albertecos <144015396+albertecos@users.noreply.github.com>
2025-04-23 11:17:13 +0200

    created Game and SpringConfig classes

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/SpringConfig.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/SpringConfig.java
@@ -1,4 +1,8 @@
 package dk.sdu.mmmi.cbse.main;
 
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringConfig {
 }
--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module Core {
     requires CommonBullet;
     requires javafx.graphics;
     requires javafx.controls;
+    requires spring.context;
     opens dk.sdu.mmmi.cbse.main to javafx.graphics;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
