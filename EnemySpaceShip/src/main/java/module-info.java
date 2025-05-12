427c22b1f64e55ab7425bdfae9e43d8225a7be67
albertecos <144015396+albertecos@users.noreply.github.com>
2025-05-12 17:59:34 +0200

    update

--- EnemySpaceShip/src/main/java/module-info.java
+++ EnemySpaceShip/src/main/java/module-info.java
@@ -1,14 +1,8 @@
-
-import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
-import dk.sdu.mmmi.cbse.enemyspaceshipsystem.EnemySpaceShipPlugin;
-import dk.sdu.mmmi.cbse.enemyspaceshipsystem.EnemySpaceShipControlSystem;
-
 module EnemySpaceship {
     requires Common;
     requires CommonBullet;
     uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
-    provides IGamePluginService with EnemySpaceShipPlugin;
-    provides IEntityProcessingService with EnemySpaceShipControlSystem;
+    provides dk.sdu.mmmi.cbse.common.services.IGamePluginService with dk.sdu.mmmi.cbse.enemyspaceshipsystem.EnemySpaceShipPlugin;
+    provides dk.sdu.mmmi.cbse.common.services.IEntityProcessingService with dk.sdu.mmmi.cbse.enemyspaceshipsystem.EnemySpaceShipControlSystem;
 
 }
