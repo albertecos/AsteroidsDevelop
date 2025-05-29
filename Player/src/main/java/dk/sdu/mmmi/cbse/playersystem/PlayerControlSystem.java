2f05c385e263a937bff879566f2f2bcc7616ace1
albertecos <144015396+albertecos@users.noreply.github.com>
2025-05-29 14:59:39 +0200

    update

--- Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerControlSystem.java
+++ Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerControlSystem.java
@@ -6,15 +6,13 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.GameKeys;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 import java.util.Collection;
 import java.util.ServiceLoader;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
-public class PlayerControlSystem implements IEntityProcessingService, IPostEntityProcessingService {
+public class PlayerControlSystem implements IEntityProcessingService {
 
     @Override
     public void process(GameData gameData, World world) {
