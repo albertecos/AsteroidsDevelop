9a46bba4dd034c411f10fdfbed4a216bdd75d716
albertecos <144015396+albertecos@users.noreply.github.com>
2025-05-30 15:52:44 +0200

    update

--- Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteriods/AsteroidPlugin.java
+++ Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteriods/AsteroidPlugin.java
@@ -26,7 +26,7 @@ public class AsteroidPlugin implements IGamePluginService {
     private Entity createAsteroid(GameData gameData) {
         Entity asteroid = new Asteroid();
         Random rnd = new Random();
-        int size = rnd.nextInt(10) + 10;
+        int size = rnd.nextInt(10) + 20;
         asteroid.setPolygonCoordinates(size, -size, -size, -size, size, size, size);
         asteroid.setX(0);
         asteroid.setY(0);
